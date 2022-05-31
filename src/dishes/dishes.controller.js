56184a8f296a110048ecc865baa2555e6ca1c85b
mborsch <matthewmborschnack@gmail.com>
2022-05-30 20:57:45 -0500

    create and update functions, set up routes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,8 +11,6 @@ function list(req, res) {
   res.json({ data: res.locals.dish });
 }
 
-let lastDishId = dishes.reduce((maxId, dish) => Math.max(maxId, dish.id), 0);
-
 function bodyDataHas(propertyName) {
   return function (req, res, next) {
     const { data = {} } = req.body;
@@ -26,9 +24,96 @@ function bodyDataHas(propertyName) {
   };
 }
 
+function isBodyEmpty(propertyname) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyname] === "") {
+      return next({
+        status: 400,
+        message: `${propertyname} can't be empty.`,
+      });
+    }
+    next();
+  };
+}
+
+function read(req, res) {
+  res.json({ data: res.locals.dish });
+}
+
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === Number(dishId));
+  if (foundDish) {
+    res.locals.dish = foundDish;
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Dish id not found: ${dishId}.`,
+  });
+}
+
+function pricePropertyIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: `Price requires a valid number`,
+    });
+  }
+  next();
+}
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId,
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  res.status(201).json({ data: newDish });
+}
+
+function update(req, res) {
+  const dish = res.locals.dish;
+  const { data: { name, description, price, image_url } = {} } = req.body;
+
+  dish.name = name;
+  dish.description = description;
+  dish.price = price;
+  dish.image_url = image_url;
+
+  res.json({ data: dish });
+}
+
 module.exports = {
-  create: [],
-  read: [],
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    pricePropertyIsValid,
+    isBodyEmpty("name"),
+    isBodyEmpty("description"),
+    isBodyEmpty("image_url"),
+    create,
+  ],
+  read: [dishExists, read],
   list,
-  update: [],
+  update: [
+    dishExists,
+    bodyDataHas("name"),
+    bodyDataHas("description"),
+    bodyDataHas("price"),
+    bodyDataHas("image_url"),
+    pricePropertyIsValid,
+    isBodyEmpty("name"),
+    isBodyEmpty("description"),
+    isBodyEmpty("image_url"),
+    update,
+  ],
 };
